2bc6d9c51c9d7a43e628106569319bcaceb3610d
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-13 16:21:06 -0300

    Fatiamento de String por posição, exemplo com URL

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real" #Simplificando a URL para o exemplo de faciamento de string
 print(url)
+
+url_base = url[0:19] #Fatiamaneto tendo o "?" como referência, porém se adionarmos o http ou mudar a url as posições irão mudar
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
