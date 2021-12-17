4c5545016c4db06cef62e772387eceddb5357f66
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-17 09:05:40 -0300

    Utilizando o método find() para fatiar a URL dinamicamente

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
-url = "bytebank.com/cambio?moedaOrigem=real" #Simplificando a URL para o exemplo de faciamento de string
+url = "http://bytebank.com/cambio?moedaOrigem=real" #URL com o http://
 print(url)
 
-url_base = url[0:19] #Fatiamaneto tendo o "?" como referência, porém se adionarmos o http ou mudar a url as posições irão mudar
+
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao] #Fatiamaneto tendo o "?" como referência de forma dinâmica
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao+1:]#Caractere após o ? até o fim da URL
 print(url_parametros)
