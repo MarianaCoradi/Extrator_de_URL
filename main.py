765c05ef48c7df4781e1d4e0ef8fbbf6256dbe52
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-20 09:51:39 -0300

    Obtendo o valor de um paramentro dentro de uma URL, porém não funciona direito com múltiplos parâmetros

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
-url = "http://bytebank.com/cambio?moedaOrigem=real" #URL com o http://
-print(url)
-
+url = "http://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real" #URL com mais parâmetros
 
 indice_interrogacao = url.find("?")
 url_base = url[:indice_interrogacao] #Fatiamaneto tendo o "?" como referência de forma dinâmica
-print(url_base)
+
 
 url_parametros = url[indice_interrogacao+1:]#Caractere após o ? até o fim da URL
 print(url_parametros)
+
+parametro_de_busca = 'moedaOrigem'
+indice_parametro_de_busca = url_parametros.find(parametro_de_busca)
+print(indice_parametro_de_busca)
+
+indice_do_valor = indice_parametro_de_busca + len(parametro_de_busca) + 1
+valor = url_parametros[indice_do_valor:]
+print(valor)
