a9f1f4b423a34932ed65206de700241714cc631e
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-20 14:21:49 -0300

    Adicionando um conversos de dolar para real e de rela para dolar

--- extrator_url.py
+++ extrator_url.py
@@ -64,3 +64,19 @@ print(extrator_url)
 
 valor = extrator_url.get_valor_parametro("moedaDestino")
 print("\nO valor do meu parâmetro na URL: {} " .format(valor))
+
+
+# Conversão de dólar para real
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print("O valor de R$" + quantidade + " reais é igual a $" + str(valor_conversao) + " dólares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(quantidade) * VALOR_DOLAR
+    print("O valor de $" + quantidade + " dólares é igual a R$" + str(valor_conversao) + " reais.")
+else:
+    print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
