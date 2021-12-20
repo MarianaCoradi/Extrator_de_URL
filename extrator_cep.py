5e139c28058bf4d0652ed6bec08d86b3422d95fe
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-20 11:55:01 -0300

    Simplificando o padrão com quantificadores

--- extrator_cep.py
+++ extrator_cep.py
@@ -6,7 +6,8 @@ import re # Regular Expression --RegEx
 # CEP 5 dígitos + hífen (opcional) + 3 dígitos
 
 #Compilação de um padrão com o re.compile():
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]") #Coloca o ? Na frente do hifen para mostra q ele é opcional
+padrao = re.compile("[0-9]{5}[-]?[0-9]{3}") #Colocando o hifen de 0-9 nos conjuntos nos mostra que os dígitos de 0 ATÉ 9 são válidos ...
+#...e o {numero} na frente nos mostra o quantificadores, que são quantos conjuntos daquilo irá repetir
 
 #A busca do padrão feito no re.compile em um endereço com o .search()
 busca = padrao.search(endereco) #retorna Match ou None e None é naturalemnte False no if
