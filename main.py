1ce2d1a7752959119816d67595f8fbd8cb6f578b
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-20 10:12:27 -0300

    Fazendo com que funcione para url com´múltiplos parâmetros utilizando condicional e o segundo parâmetro do método .find()

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 
-url = "http://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real" #URL com mais parâmetros
+url = "http://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real" #URL com mais parâmetros
 
+# Separa base e parâmetros
 indice_interrogacao = url.find("?")
-url_base = url[:indice_interrogacao] #Fatiamaneto tendo o "?" como referência de forma dinâmica
-
-
-url_parametros = url[indice_interrogacao+1:]#Caractere após o ? até o fim da URL
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-parametro_de_busca = 'moedaOrigem'
+# Busca o valor de um parâmetro
+parametro_de_busca = 'quantidade'
 indice_parametro_de_busca = url_parametros.find(parametro_de_busca)
-print(indice_parametro_de_busca)
-
 indice_do_valor = indice_parametro_de_busca + len(parametro_de_busca) + 1
-valor = url_parametros[indice_do_valor:]
+indice_do_e_comercial = url_parametros.find("&", indice_do_valor) #Irá verificar se existe o & apartir do meu indice_do_valor, se não achar...
+#... quer dizer que é o meu ultimo parametro e o find irá retorna -1
+if indice_do_e_comercial == -1 :
+    valor = url_parametros[indice_do_valor:]
+else:
+    valor = url_parametros[indice_do_valor:indice_do_e_comercial]
 print(valor)
